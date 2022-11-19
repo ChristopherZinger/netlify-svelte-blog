f2915253b80b82c7d8146c75ec89654a49c5a48a
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-11-19 21:50:51 +0100

    create getAllTags retriever

--- src/lib/retrievers/tags.ts
+++ src/lib/retrievers/tags.ts
@@ -5,5 +5,5 @@ import { getDocs, limit, query } from 'firebase/firestore';
 export const getTags = async (): Promise<Tag_FsDoc[]> =>
 	(await getDocs(query(getTagCollectionReference(), limit(8)))).docs.map((s) => s.data());
 
-// export const getAllTags = async (): Promise<Tag_FsDoc[]> =>
-// 	(await getDocs(getTagCollectionReference())).docs.map((s) => s.data());
+export const getAllTags = async (): Promise<Tag_FsDoc[]> =>
+	(await getDocs(getTagCollectionReference())).docs.map((s) => s.data());
