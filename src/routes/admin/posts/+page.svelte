5b0e1c80e75561e9007c3ba30805d9169759e9bb
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-11-26 16:24:45 +0100

    add admin posts page

---
+++ src/routes/admin/posts/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { getPostCollectionGroupRef } from '$lib/collections';
+	import type { Post_FsDoc } from '$lib/schemas';
+	import { convertToDraft } from '$lib/utils/create-post-utils';
+	import { getFirestore, onSnapshot } from 'firebase/firestore';
+
+	let posts: Post_FsDoc[] | undefined;
+
+	if (browser) {
+		onSnapshot(getPostCollectionGroupRef(), (s) => {
+			posts = s.docs.map((d) => d.data());
+		});
+	}
+</script>
+
+<h1 class="h5">Posts</h1>
+
+{#if posts}
+	{#if posts.length}
+		<ul>
+			{#each posts as post}
+				<li class="flex justify-between">
+					<div>{post.title}</div>
+					<button on:click={() => convertToDraft(getFirestore(), post.slug)}
+						>convert to draft</button
+					>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		No posts here
+	{/if}
+{:else}
+	<spinner />
+{/if}
