fe6aa4df7e4a841dd36efdddd8b40f51bc6b0762
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-11-26 16:24:28 +0100

    add draft to navigation

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -7,9 +7,9 @@
 		<nav class=" lg:col-start-2 lg:col-span-11 my-10">
 			<ul class="flex gap-x-8 underline">
 				<li><a href="/admin">admin</a></li>
-				<li><a href="/admin/create-draft">create new draft</a></li>
-				<li><a href="/admin/draft-list">drats list</a></li>
-				<!-- <li><a href="/admin/posts">posts</a></li> -->
+				<li><a href="/admin/create-draft">new draft</a></li>
+				<li><a href="/admin/draft-list">drafts</a></li>
+				<li><a href="/admin/posts">posts</a></li>
 				<!-- <li><a href="/admin/draft">series</a></li>  -->
 			</ul>
 		</nav>
