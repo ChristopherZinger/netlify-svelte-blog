ac9284c4493bd2acf7ff3d45c60b95ea1e00dfcf
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-11-27 20:00:49 +0100

    fix build error

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,11 @@
 import { getFirebaseConf, TIER } from '$lib/firebase';
 import { initializeApp } from 'firebase/app';
-import { PUBLIC_STAGING_OR_PROD } from '$env/static/public';
+import * as publicData from '$env/static/public';
 import { z } from 'zod';
 
 /** @type {import('./$types').LayoutLoad} */
 export function load() {
+	const { PUBLIC_STAGING_OR_PROD } = publicData as any;
 	const tier = z.nativeEnum(TIER).parse(PUBLIC_STAGING_OR_PROD);
 	const firebaseConfig = getFirebaseConf(tier);
 	initializeApp(firebaseConfig);
