bf58e6fe29c8b1c06501f31c95a825eb3116e19f
christopher z <krzysztof.zinger@gmail.com>
2024-10-25 16:37:08 +0200

    use new domain

--- src/lib/wordpress/url.ts
+++ src/lib/wordpress/url.ts
@@ -1,4 +1,4 @@
-export const wordpressApiUrl = new URL('https://staging.ciezar-architektury.pl/wp-json/wp/v2/');
+export const wordpressApiUrl = new URL('https://api.byte-increments.dev/wp-json/wp/v2/');
 
 export function appendPathItemToUrl(url: URL, chunk: string) {
 	url.pathname = url.pathname + chunk + '/';
