ffd540b96098a946fa869175681356838359f967
christopher z <krzysztof.zinger@gmail.com>
2025-07-27 11:59:54 +0200

    update api urls

--- src/lib/wordpress/url.ts
+++ src/lib/wordpress/url.ts
@@ -1,5 +1,5 @@
 export const wordpressApiUrl = new URL(
-	'https://api.byte-increments.dev/wp-json/wp/v2/'
+	'https://api.exclusive-or.dev/wp-json/wp/v2/'
 );
 
 export function appendPathItemToUrl(url: URL, chunk: string) {
