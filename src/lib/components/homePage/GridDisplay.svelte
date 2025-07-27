540f799f39d919f7d6b46cd0eb7fd9f28dcd8915
christopher z <krzysztof.zinger@gmail.com>
2025-07-27 08:53:12 +0200

    update home page layout

--- src/lib/components/homePage/GridDisplay.svelte
+++
@@ -1,7 +0,0 @@
-<div class="mb-12 pb-12 border-b-black">
-	<div class="grid grid-cols-1 lg:grid-cols-4 gap-y-8">
-		<div class="lg:col-span-2 lg:col-start-2">
-			<slot />
-		</div>
-	</div>
-</div>
--- src/lib/components/homePage/HomePage.svelte
+++ src/lib/components/homePage/HomePage.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import TopLevelMarginContainer from '$lib/components/containers/TopLevelMarginContainer.svelte';
 	import TagList from '$lib/components/TagList.svelte';
-	import GridDisplay from '$lib/components/homePage/GridDisplay.svelte';
 	import Jumbotron from '$lib/components/homePage/Jumbotron.svelte';
 	import type { Post_WP, Tag_WP } from '$lib/schemas';
 	import PostListItem from './PostListItem.svelte';
@@ -34,32 +33,38 @@
 
 <TopLevelMarginContainer>
 	<div class="mt-14">
-		<GridDisplay>
-			<div class="flex flex-col gap-6">
-				<div class="pb-1 border-b border-black">
-					<h2 class="text-2xl font-bold">Logbook</h2>
+		<div class="mb-12 pb-12 border-b-black">
+			<div class="grid grid-cols-1 lg:grid-cols-12 gap-y-8">
+				<div class="lg:col-span-6 lg:col-start-2 mb-12">
+					<div class="flex flex-col gap-6">
+						<div class="pb-1 border-b border-black">
+							<h2 class="text-2xl font-bold">Logbook</h2>
+						</div>
+
+						<div
+							class="flex flex-col gap-6 max-h-[600px] overflow-y-scroll"
+						>
+							{#each logBooks as logBook}
+								<PostListItem post={logBook} />
+							{/each}
+						</div>
+					</div>
 				</div>
 
-				<div
-					class="flex flex-col gap-6 max-h-[600px] overflow-y-scroll"
-				>
-					{#each logBooks as logBook}
-						<PostListItem post={logBook} />
-					{/each}
-				</div>
+				<div class="lg:col-span-6 lg:col-start-4">
+					<div class="pb-1 border-b border-black">
+						<h2 class="text-2xl font-bold">Latest Posts</h2>
+					</div>
 
-				<div class="pb-1 border-b border-black">
-					<h2 class="text-2xl font-bold">Latest Posts</h2>
+					<div
+						class="flex flex-col gap-6 max-h-[600px] overflow-y-auto"
+					>
+						{#each shownPosts as post}
+							<PostListItem {post} />
+						{/each}
+					</div>
 				</div>
-
-				<div
-					class="flex flex-col gap-6 max-h-[600px] overflow-y-auto"
-				>
-					{#each shownPosts as post}
-						<PostListItem {post} />
-					{/each}
-				</div>
-			</div></GridDisplay
-		>
+			</div>
+		</div>
 	</div>
 </TopLevelMarginContainer>
